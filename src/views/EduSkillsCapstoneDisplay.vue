759fd60d26e72ce6a85cc9063310edc7e115b268
Tyler Fisher <tfisher21@gmail.com>
2018-12-03 21:16:50 -0600

    Update Education information

--- src/views/EduSkillsCapstoneDisplay.vue
+++ src/views/EduSkillsCapstoneDisplay.vue
@@ -1,6 +1,76 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <div class="container">
+      <h1 class="mb-2">{{ message }}</h1>
+    </div>
+    <div class="container border">
+      <h2 class="mt-2">Education</h2>
+      <form class="form">
+        <div class="form-group row">
+          <label class="col-form-label col">Start Date:</label>
+          <label class="col-form-label col">{{
+            educationInfo.startDate
+          }}</label>
+          <div class="col">
+            <input
+              class="form-control"
+              type="text"
+              v-model="educationInfo.startDate"
+            />
+          </div>
+          <div class="col"><input class="btn btn-primary" type="submit" /></div>
+        </div>
+        <div class="form-group row">
+          <label class="col-form-label col">End Date:</label>
+          <label class="col-form-label col">{{ educationInfo.endDate }}</label>
+          <div class="col">
+            <input
+              class="form-control"
+              type="text"
+              v-model="educationInfo.endDate"
+            />
+          </div>
+          <div class="col"><input class="btn btn-primary" type="submit" /></div>
+        </div>
+        <div class="form-group row">
+          <label class="col-form-label col">Degree:</label>
+          <label class="col-form-label col">{{ educationInfo.degree }}</label>
+          <div class="col">
+            <input
+              class="form-control"
+              type="text"
+              v-model="educationInfo.degree"
+            />
+          </div>
+          <div class="col"><input class="btn btn-primary" type="submit" /></div>
+        </div>
+        <div class="form-group row">
+          <label class="col-form-label col">University:</label>
+          <label class="col-form-label col">{{
+            educationInfo.university
+          }}</label>
+          <div class="col">
+            <input
+              class="form-control"
+              type="text"
+              v-model="educationInfo.university"
+            />
+          </div>
+
+        </div>
+      </form>
+    </div>
+    <div class="container border">
+      <h2 class="mt-2">Skills</h2>
+      <form class="form">
+        <div class="form-group row">
+          <label class="col-form-label col">Skills:</label>
+          <div class="col">
+            <textarea v-model="skills" v-bind:placeholder="skills"></textarea>
+          </div>
+        </div>
+        <div class="col"><input class="btn btn-primary" type="submit" /></div>
+      </form>
   </div>
 </template>
 
@@ -12,7 +82,14 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Education, Skills, & Capstone Info",
+      educationInfo: {
+        startDate: "09-01-2018",
+        endDate: "05-31-2022",
+        degree: "Computer Science",
+        university: "DePaul University"
+      },
+      skills: "Ruby"
     };
   },
   created: function() {
@@ -20,7 +97,6 @@ export default {
     axios.get("localhost:3000/api/students").then(response => {});
 
     /* Will go into .then */
-    this.message = "Test";
   },
   methods: {},
   computed: {}
