4c60821e175e2e587b7250efafe9da6a119fc71b
danielmuschler <dmusch4@gmail.com>
2018-12-03 20:33:12 -0600

    education info

--- src/router.js
+++ src/router.js
@@ -1,23 +1,29 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+import Experience from "./views/Experience.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/experience",
+      name: "experience",
+      component: Experience
+    },
+    {
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
---
+++ src/views/Experience.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Start Date</h1>
+    <input v-model="Start_Date" />
+    <h1>End Date</h1>
+    <input v-model="End_Date" />
+    <h1>Job Title</h1>
+    <input v-model="Job_Title" />
+    <h1>Company Name</h1>
+    <input v-model="Company_Name" />
+    <h1>Details</h1>
+    <input v-model="Details" />
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      Start_Date: 2010,
+      End_Date: 2013,
+      Job_Title: "Temp Job",
+      Company_Name: "Actualize",
+      Details: "These are the details"
+    };
+  }
+};
+// // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+// export default {
+//   name: "home",
+//   components: {
+//     HelloWorld
+//   }
+// };
+</script>
