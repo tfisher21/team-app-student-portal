37df4054c8379e2b176073564456752aff743900
Tyler Fisher <tfisher21@gmail.com>
2018-12-03 20:15:32 -0600

    Initial Commit. Create EduSkillsCapsoneDisplay.vue and link in router and navbar.

---
+++ src/views/EduSkillsCapstoneDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {
+    /* HOLD - Waiting for backend deployment */
+    axios.get("localhost:3000/api/students").then(response => {});
+
+    /* Will go into .then */
+    this.message = "Test";
+  },
+  methods: {},
+  computed: {}
+};
+</script>
